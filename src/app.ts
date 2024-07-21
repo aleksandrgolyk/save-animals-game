523cc02527bac7fad3218f801d2bfb5fe28a00bf
Oleksandr Holyk <aleksandr.golyk@gmail.com>
2024-07-22 01:42:02 +0200

    [MINOR] update field color

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,6 @@ import * as PIXI from "pixi.js";
 export const app = new PIXI.Application({
   width: 800,
   height: 600,
-  backgroundColor: 0x00ff00,
+  backgroundColor: 0x0a8e0a,
   view: document.getElementById("game-canvas") as HTMLCanvasElement,
 });
