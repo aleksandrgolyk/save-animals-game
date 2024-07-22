f6a99b976f7751be5fc798060be68c16e7f0916c
Oleksandr Holyk <aleksandr.golyk@gmail.com>
2024-07-22 18:00:40 +0200

    [MINOR] removed unnecessary code

--- src/utils/randomPosition.ts
+++ src/utils/randomPosition.ts
@@ -1,16 +1,6 @@
 import { Position } from "../models/position";
 import { yard } from "../objects/yard";
 
-// valid position is when the animal is not in the yard and outside of the game area
-const isPositionValid = (x: number, y: number, animalSize: number): boolean => {
-  return (
-    x + animalSize < yard.x ||
-    x > yard.x + yard.width ||
-    y + animalSize < yard.y ||
-    y > yard.y + yard.height
-  );
-};
-
 // Function to generate random positions
 export const getRandomPosition = (
   maxWidth: number,
@@ -19,12 +9,21 @@ export const getRandomPosition = (
 ): Position => {
   let x: number = 0;
   let y: number = 0;
+  let validPosition = false;
 
-  // Continue generating random positions until a valid one appears
-  while (isPositionValid(x, y, animalSize)) {
+  while (!validPosition) {
     // take a random position within the game area but with extra space - not to close
     x = Math.random() * (maxWidth - 3 * animalSize) + animalSize;
     y = Math.random() * (maxHeight - 3 * animalSize) + animalSize;
+    // we have to check if the animal spawn place is not in the yard and outside of the game area
+    if (
+      x + animalSize < yard.x ||
+      x > yard.x + yard.width ||
+      y + animalSize < yard.y ||
+      y > yard.y + yard.height
+    ) {
+      validPosition = true;
+    }
   }
 
   return { x, y };
